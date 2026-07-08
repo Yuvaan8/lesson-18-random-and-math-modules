8f211bd30daf39ca78e83622da52805cb1dc4d02
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-07-09 01:05:30 +0530

    initial commit

---
+++ sign.py
@@ -0,0 +1,10 @@
+import math
+x = 5
+y = -2
+print('Ceiling', math.ceil(x))
+print('Floor', math.floor(x))
+print('copysign', math.copysign(x,y))
+print('exponential', math.exp(x))
+print('Factorial', math.factorial(x))
+print('IsNan', math.isnan(x))
+print('Degrees', math.degrees(x))
